8d869cf41ddb66a51f9b35123d765b5d425f0c97
PustovayaTS <ais_ta@bk.ru>
2022-10-30 20:16:25 +0700

    Задание № 2 блок 2. Введение в коллекции. Хеш и хеш-функции. Множества.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList <Integer> intList = new ArrayList<>();
+        Random r = new Random();
+        int x = r.nextInt(1000) ;
+        for (int i = 0; i<=20; i++){
+            intList.add(x);
+            x = r.nextInt(1000);
+        }
+        System.out.println(intList);
+        int i = 0;
+        System.out.println();
+        while (i != intList.size()){
+            if (intList.get(i) % 2 != 0){
+                intList.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.println(intList);
     }
 }
